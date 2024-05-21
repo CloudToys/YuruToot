0d95e24b2e88a868069037e1e2dcf3ba8bbd07b9
무라쿠모 <space@psec.dev>
2024-05-22 01:30:52 +0900

    fix: maybe problem?

--- app/javascript/flavours/glitch/components/logo.tsx
+++ app/javascript/flavours/glitch/components/logo.tsx
@@ -1,10 +1,7 @@
 import logo from '@/images/logo.svg';
 
 export const WordmarkLogo: React.FC = () => (
-  <img src='https://squares.puyopuyotetr.is/logo.png' className='logo logo--wordmark' alt='Mastodon Logo'>
-    <title>Mastodon</title>
-    <use xlinkHref='#logo-symbol-wordmark' />
-  </img>
+  <img src='https://squares.puyopuyotetr.is/logo.png' className='logo logo--wordmark' alt='Mastodon' />
 );
 
 export const SymbolLogo: React.FC = () => (
